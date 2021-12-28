695523262b7ddc35b4f0046b909303f14d5947d2
yuvipriya22 <96561153+yuvipriya22@users.noreply.github.com>
2021-12-28 18:31:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,107 +1,18 @@
-Survey
-    .StylesManager
-    .applyTheme("modern");
+function check() 
+  
+{
 
-var json = {
-    title: "GK Question",
-    firstPageIsStarted: true,
-    startSurveyText: "Start Quiz",
-    pages: [
-        {
-            "elements": [
-                {
-                    "name": "name",
-                    "type": "text",
-                    "title": "Please enter your name:",
-                    "placeHolder": "Please enter your name",
-                    "isRequired": true,
-                    "autoComplete": "name"
-                }
-            ]
-            },
-       
-        {
-            "elements": [
-                {
-                    "type": "html",
-                    "html": "The SRM University .",
-                }
-            ]
-        }, 
-        
-        {
-            "elements": [
-                {
-                    "type": "radiogroup",
-                    "name": "India won the Lord's Test",
-                    "title": "India won the Lord's Test on August 16, 2021, defeating England by 151 runs. It was India's ____________ victory at Lords against the hosts",
-                    "choices": [
-                        "4th", "3rd", "5th", "6th"
-                    ],
-                    "correctAnswer": "3rd"
-                }
-            ]
-        }, {
-            "elements": [
-                {
-                    "type": "radiogroup",
-                    "name": "Tokyo Olympic 2020",
-                    "title": "Which of the following players won maximum gold medals in the Tokyo Olympic 2020?'",
-                    "choicesOrder": "random",
-                    "choices": [
-                        "Cealeb Dressel (USA)", "Emma Mckeon (Australia)", "Daiki Hashimoto (Japan)", "Vitalina Batsarashkina (Russia)"
-                    ],
-                    "correctAnswer": "Cealeb Dressel (USA)"
-                }
-            ]
-        }, {
-            "elements": [
-                {
-                    "type": "radiogroup",
-                    "name": "Hockey",
-                    "title": "Which of the following states sponsored India's Men's Hockey Team and Women's Hockey Team?",
-                    "choicesOrder": "random",
-                    "choices": [
-                        "Punjab", "Odisha", "Haryana", "Rajasthan"
-                    ],
-                    "correctAnswer": "Odisha"
-                }
-            ]
-            
-        },
-        {
-            "elements": [
-                {
-                    "type": "radiogroup",
-                    "name": "Tokyo Olympic 2020",
-                    "title": "Which of the following players won maximum gold medals in the Tokyo Olympic 2020?'",
-                    "choicesOrder": "random",
-                    "choices": [
-                        "Cealeb Dressel (USA)", "Emma Mckeon (Australia)", "Daiki Hashimoto (Japan)", "Vitalina Batsarashkina (Russia)"
-                    ],
-                    "correctAnswer": "Cealeb Dressel (USA)"
-                }
-            ]
-        },
-        
-    ],
-    completedHtml: "<h4>You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>"
-};
-
-
-window.survey = new Survey.Model(json);
-
-survey
-    .onComplete
-    .add(function (sender) {
-        localStorage.setItem("final", JSON.stringify(sender.data));
-        console.log(sender.data);
-        document
-            .querySelector('#surveyResult')
-            .textContent = "Result JSON:\n" + JSON.stringify(sender.data, null, 3);
-            
-    });
-
-$("#surveyElement").Survey({model: survey});
-
-console.log(localStorage.getItem("final"));
+    var c=0;
+    var q= document.quiz.value;
+    var q1 = document.quiz.q1.value;
+    var q2 = document.quiz.q2.value;
+    var q3 = document.quiz.q3.value;
+    var q4 = document.quiz.q4.value;
+    var q5 = document.quiz.q5.value;
+    if(q1=="3rd"){c++;}
+    if(q2=="Cealeb Dressel (USA)"){c++;}
+    if(q3=="Odisha"){c++;}
+    if(q4=="Vandana Kataria"){c++;}
+    if(q5=="China"){c++;}
+alert("the correct answer is"+c+"/5");   
+}
